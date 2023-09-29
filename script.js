2d7e5b2283a7097375f8a0c9ac52d3471c9b999f
Ajay309 <57127889+Ajay309@users.noreply.github.com>
2023-09-29 16:05:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	// Users can skip the loading process if they want.
+	$('.skip').click(function() {
+		$('.overlay, body').addClass('loaded');
+	})
+	
+	// Will wait for everything on the page to load.
+	$(window).bind('load', function() {
+		$('.overlay, body').addClass('loaded');
+		setTimeout(function() {
+			$('.overlay').css({'display':'none'})
+		}, 2000)
+	});
+	
+	// Will remove overlay after 1min for users cannnot load properly.
+	setTimeout(function() {
+		$('.overlay, body').addClass('loaded');
+	}, 60000);
+})
